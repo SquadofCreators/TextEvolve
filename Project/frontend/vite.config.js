8ca2122171e4529d2a0f34807d9827716e2dadb9
Sibi Siddharth S <sibisiddharth008@gmail.com>
2025-03-11 23:33:23 +0530

    deployment check

--- Project/frontend/vite.config.js
+++ Project/frontend/vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/TextEvolve/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base:'/TextEvolve/'
 })
